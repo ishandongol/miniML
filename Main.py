044f6dd7edaa2c4585c928ba77c44f1eae23f77d
ishandongol <ishandongol@gmail.com>
2018-08-22 12:31:20 +0545

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+
+from miniML.MiniML import MiniML
+
+boston = load_boston()
+bostonData = pd.DataFrame(boston.data)
+bostonData.columns = boston.feature_names
+bostonData['PRICE'] = boston.target
+bostonData = (bostonData - bostonData.mean()) / bostonData.std()
+
+print(bostonData.head())
+
+Y = bostonData.iloc[:,13:14].values
+X = bostonData.iloc[:,0:13]
+ones = np.ones([len(X),1])
+X = np.concatenate((ones,X), axis = 1)
+
+X_train, X_rest, Y_train, Y_rest = train_test_split(X,Y,test_size=0.4)
+X_test,X_validate,Y_test,Y_validate = train_test_split(X_rest,Y_rest,test_size=0.5)
+
+print('Training Data: \t',len(X_train))
+print('Validation Data: \t',len(X_validate))
+print('Test Data: \t',len(X_test))
+
+alpha = 0.001 #? Learning Rate
+lamda = 0.1 #? regularization rate
+max_iterations = 10000
+W = np.zeros((1,len(X_train[0,:])))
+
+miniML = MiniML()
+Weight, Cost = miniML.train(X_train,Y_train,W,alpha,max_iterations,lamda)
+
+w = Weight.flatten()
+pandasW = pd.DataFrame(Weight.T)
+pandasW.columns = ["Theta"]
+print(pandasW)
+
+plt.scatter(Y_train,miniML.getHypothesis())
+plt.xlabel("Prices: $Y_I$")
+plt.ylabel("Predicted Prices: $\hat{y}_i$")
+plt.title("Prices vs predicted prices: $Y_i$ vs $\hat{y}_i$ (Training)")
+
+print('Cost')
+print('')
+print("Training Cost \t\t", Cost[len(Cost)-1])
+print('')
+
+cost_validation = miniML.validate(X_validate,Y_validate,Weight)
+print("Validation Cost \t",cost_validation)
+print('')
+plt.scatter(Y_validate,miniML.getHypothesis())
+plt.xlabel("Prices: $Y_I$")
+plt.ylabel("Predicted Prices: $\hat{y}_i$")
+plt.title("Prices vs predicted prices: $Y_i$ vs $\hat{y}_i$ (Validation)")
+
+cost_test = miniML.test(X_test,Y_test,Weight)
+print("Test Cost \t\t",cost_test)
+plt.scatter(Y_test,miniML.getHypothesis(),color='g')
+plt.xlabel("Prices: $Y_I$")
+plt.ylabel("Predicted Prices: $\hat{y}_i$")
+plt.title("Prices vs predicted prices: $Y_i$ vs $\hat{y}_i$ (Test)")
+plt.show()
---
+++ miniML/MiniML.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class MiniML:
+
+    __hypothesis = None
+
+    def __init__(self):
+        print("MiniML")
+
+    def linearEquation(self,X,W):
+        self.__hypothesis = X @ W.T
+        return self.__hypothesis
+
+    def calculateCost(self,X,Y,W,lamda=0):
+        inner = np.power(((self.linearEquation(X,W))-Y),2) + (lamda * np.sum(np.power(W,2)))
+        return (np.sum(inner)/(2*len(X)))
+
+
+    def linearRegression(self,X,Y,W,alpha,lamda,max_iterations):
+        cost = np.zeros(max_iterations)
+
+        for i in range(max_iterations):
+            W = W - (alpha/len(X)) * (np.sum(X * (X @ W.T - Y), axis=0)+(lamda * W))
+            cost[i] = self.calculateCost(X,Y,W,lamda)
+            if(i % 1000 == 0):
+                print("Cost", cost[i])
+
+        return W,cost
+
+    def getHypothesis(self):
+        return self.__hypothesis
+
+    def train(self,X,Y,W,alpha,max_iterations,lamda=0):
+        return self.linearRegression(X,Y,W,alpha,lamda,max_iterations)
+
+    def validate(self,X,Y,W):
+        return self.calculateCost(X,Y,W)
+
+    def test(self,X,Y,W):
+        return self.validate(X,Y,W)
